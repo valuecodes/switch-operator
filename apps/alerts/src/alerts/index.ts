b20115495546a5a3d38736de7aedf281eff0fea2
Juha Kangas <42040080+valuecodes@users.noreply.github.com>
2026-07-12 11:06:00 +0300

    feat: add sp500 drawdown-from-ATH threshold alert

--- apps/alerts/src/alerts/index.ts
+++ apps/alerts/src/alerts/index.ts
@@ -1,11 +1,12 @@
 import { healthcheck } from "./healthcheck";
 import { sp500Close } from "./sp500-close";
+import { sp500Drawdown } from "./sp500-drawdown";
 import type { Alert } from "./types";
 
 /**
  * The registry of alerts. Add new alerts here; remember to also add each
  * alert's `cron` to `wrangler.jsonc` `triggers.crons`.
  */
-const alerts: Alert[] = [healthcheck, sp500Close];
+const alerts: Alert[] = [healthcheck, sp500Close, sp500Drawdown];
 
 export { alerts };
